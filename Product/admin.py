a37ccfea66cf5157d568563206e59e5b59044adf
Stanley Chidolue <chidoluechibueze@gmail.com>
2024-03-24 17:40:49 +0100

    customized the admin page

--- Product/admin.py
+++ Product/admin.py
@@ -6,5 +6,21 @@ from .models import Products
 
 class ProductAdmin(admin.ModelAdmin):
     list_display=("product_name","product_price")
+    list_filter=("product_gender","prod_is_featured","collection_id")
+    search_fields=("product_name__contains",)
+    list_per_page=20
+    fields=("product_name","product_description","product_price",("product_img_1",'product_img_2','product_img_3'),"collection",("product_gender","prod_is_featured"))
+    actions=("set_prod_to_featured","set_prod_to_unfeatured")
+
+    def set_prod_to_featured(self,request,queryset):
+        queryset.update(prod_is_featured=True)
+        self.message_user(request, "The selected product(s) has been Featured")
+    set_prod_to_featured.short_description="Set Selected Product as Featured"
+
+
+    def set_prod_to_unfeatured(self,request,queryset):
+        queryset.update(prod_is_featured=False)
+        self.message_user(request, "The selected product(s) has been Unfeatured")
+    set_prod_to_unfeatured.short_description="Set Selected Product as Unfeatured"
 
 admin.site.register(Products,ProductAdmin)
--- Store/admin.py
+++ Store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Advertisement
+from .models import Advertisement,Collection
 
 # Register your models here.
 
@@ -8,4 +8,10 @@ class AdvertAdmin(admin.ModelAdmin):
     list_display=("advert_title","advert_location")
 
 
+class CollectionAdmin(admin.ModelAdmin):
+    list_display=("collection_name","id")
+    search_fields=("collection_name__contains",)
+
+
 admin.site.register(Advertisement,AdvertAdmin)
+admin.site.register(Collection,CollectionAdmin)
