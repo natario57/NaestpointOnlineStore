dbe71e983ac72303e2f88fa340debf3dfba4d905
Stanley Chidolue <chidoluechibueze@gmail.com>
2024-03-27 23:08:04 +0100

    created forget password functionality

--- User/urls.py
+++ User/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
     path("profile/", views.user_profile, name="user-profile"),
     # path("logout/", views.logout_page, name="logout-page"),
     path('logout/',auth_views.LogoutView.as_view(),name="logout-page"),
+     path('password-reset/',auth_views.PasswordResetView.as_view(template_name="user/password_reset.html"),name='password-reset'),
+    path('password-reset/done/',auth_views.PasswordResetDoneView.as_view(template_name="user/password_reset_done.html"),name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="user/password_reset_confirm.html"),name='password_reset_confirm'),
+    path('password-reset-complete/',auth_views.PasswordResetCompleteView.as_view(template_name="user/password_reset_complete.html"),name='password_reset_complete'),
     
     
 ]
